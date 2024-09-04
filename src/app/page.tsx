478016b4c22ea5cab36e512a5b582fac96123979
DeluluSaro <166493878+DeluluSaro@users.noreply.github.com>
2024-09-05 00:31:30 +0530

    spinner_done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,8 @@ import { z } from "zod";
 
 import UploadButton from "./upload-button";
 import FileCard from "./file-card";
+import Image from "next/image";
+import { Hospital, SparklesIcon, SplineIcon } from "lucide-react";
 
 
 
@@ -38,17 +40,45 @@ export default function Home() {
 
   return (
     <main className="container mx-auto pt-12">
-      <div className="flex justify-between items-center mb-8">
-        <h1 className="text-4xl font-bold">Your Reports</h1>
+      
 
-        <UploadButton></UploadButton>
+    {files === undefined && (
+      <div className="flex flex-col justify-center items-center mt-10"><Hospital className="w-20 h-20 animate-bounce"></Hospital>
+      <h1 className="text-2xl">Please wait until we load</h1></div>
+    )}
+
+     
+     {files && files?.length===0 && (
+      <div className="flex flex-col w-[1275px] mr-10  items-center gap-4 mt-10 ">
+      <Image src={require('./../../public/nothing.svg')} alt="nothing" width={100} height={100} className="w-[250px] h-[250px] "></Image>
+      <h1 className="text-2xl font-bold">No Records Found</h1>
+      <UploadButton></UploadButton>
       </div>
+     )}
 
-    <div className="grid grid-cols-4 gap-4">
-    {files?.map((file) => {
-        return <FileCard key={file._id} file={file}></FileCard>;
-      })}
+     {files && files.length>0 && (
+      <>
+      <div className="flex justify-between items-center mb-8">
+      <h1 className="text-4xl font-bold">Your Reports</h1>
+
+      <UploadButton></UploadButton>
     </div>
+      <div className="grid grid-cols-4 gap-4">
+      
+
+    
+      {files?.map((file)=>{
+        return <FileCard key={file._id} file={file}></FileCard>
+      })}
+    
+    </div>
+      </>
+     )}
+
+
+     
+    
+    
     </main>
   );
 }
