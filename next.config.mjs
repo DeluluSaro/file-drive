1dcfa0e2c50717c25e6fa3aad5d6c99b4b8f1aba
DeluluSaro <166493878+DeluluSaro@users.noreply.github.com>
2024-09-17 14:37:57 +0530

    Final_before

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images:{
+        remotePatterns:[
+            {
+                hostname:'giddy-minnow-853.convex.cloud'
+            }
+        ]
+    }
+};
+
+export default nextConfig;
